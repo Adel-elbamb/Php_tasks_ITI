4a1a7f192ae05ccb0c8dcca0802ea61e780ba1af
Adel-elbamb <engadel987654321@gmail.com>
2025-02-20 17:21:48 +0200

    tasks_php

---
+++ day_1/task_1/data.php
@@ -0,0 +1,9 @@
+<?php 
+echo "Name is: ".$_POST["firstName"]."<br>";
+echo "Country is: ".$_POST["country"]."<br>";
+echo "Gender is: ".$_POST["gender"]."<br>";
+// echo "Skill is: ".$_POST["bids"]."<br>";
+echo "User Name is: ".$_POST["username"]."<br>";
+echo "Password is: ".$_POST["password"]."<br>";
+echo "Department is: ".$_POST["department"]."<br>";
+?>
